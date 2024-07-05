19c5925d926e14e899101ed027c6b584d973e058
David Cruz <dcruzer92@gmail.com>
2024-07-04 23:14:48 -0600

    Home component updated

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -5,8 +5,40 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './home.component.html',
-  styles: ``
-})
-export class HomeComponent {
+  styles: `
+    .card {
+      position: relative;
+      overflow: hidden;
+      cursor: pointer;
+    }
+    
+    .card img {
+      transition: opacity 0.3s ease;
+    }
+    
+    .card .overlay {
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      background-color: rgba(0, 0, 0, .5);
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      color: #ffffff;
+      font-size: 1.25rem;
+      opacity: 0;
+      transition: opacity 0.3s ease;
+    }
+        
+    .card:hover img {
+      opacity: 0.5;
+    }
 
-}
+    .card:hover .overlay {
+      opacity: 1;
+    }
+  `
+})
+export class HomeComponent {}
